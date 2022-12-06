8d6e4bb213dd3067077c08714ac1e3962505205c
Leonardo <leonardotandelicw@outlook.com>
2022-12-06 00:52:31 -0300

    fix db connection

--- src/api.js
+++ src/api.js
@@ -1,5 +1,12 @@
 import express from "express";
 import routes from "./routes/index.js"
+import db from "./config/dbConnect.js";
+
+db.on("error", console.log.bind(console, 'Erro de Conexão'))
+db.once("open", () => {
+  console.log("conexão com o banco feita com sucesso")
+})
+
 
 const api = express();
 
