e9fc84a11d269dac9fda895017aa58f9c5a36200
Leonardo <leonardotandelicw@outlook.com>
2022-12-17 12:23:28 -0300

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,16 +1,21 @@
 import express from "express";
 import routes from "./routes/index.js"
+import cors from "cors";
 import db from "./config/dbConnect.js";
 
+//abre conexão com o banco
 db.on("error", console.log.bind(console, 'Erro de Conexão'))
 db.once("open", () => {
   console.log("conexão com o banco feita com sucesso")
 })
 
-
+//express
 const api = express();
 
-api.use(express.json())
+//define retorno dos dados em json 
+api.use(express.json(), cors())
+
+//define as rotas
 routes(api)
 
 export default api
