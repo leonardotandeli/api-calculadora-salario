c85ee3bdf9dee8cd8f67ee7d74914f1fd2585229
Leonardo <leonardotandelicw@outlook.com>
2022-12-17 12:44:02 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import api from "./src/api.js";
 
 //define a porta da api
-const port = process.env.port || 3001;
+const port = process.env.port
 
 //inicia o servidor http
 api.listen(port, () => {
