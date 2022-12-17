28684837c9c1ef5f22d5b9ac05c35882c26f3c33
Leonardo <leonardotandelicw@outlook.com>
2022-12-17 12:23:53 -0300

    Update salarioRoutes.js

--- src/routes/salarioRoutes.js
+++ src/routes/salarioRoutes.js
@@ -3,7 +3,9 @@ import salarioController from "../controllers/salarioController.js";
 
 const router = express.Router()
 
+//define as rotas da API
 router
 .post('/salario', salarioController.calcularSalarioLiquido)
+.get('/salario', salarioController.exibirSalarios)
 
 export default router
