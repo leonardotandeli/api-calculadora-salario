d473f6b51ce9309e1b8fa3955e90b3b5305dcbd4
Leonardo <leonardotandelicw@outlook.com>
2022-12-17 12:23:45 -0300

    Update Salario.js

--- src/models/Salario.js
+++ src/models/Salario.js
@@ -1,13 +1,20 @@
 import mongoose, { SchemaTypes } from "mongoose";
 
+//define um modelo de salario para amarzenamento no mongodb
 const salarioSchema = new mongoose.Schema(
     {
         id: {type: String},
         salarioBruto: {type: Number},
         dataDeCalculo: {type: Date, default: Date.now},
-        totalDescontos: {type: Number},
+        dependentes: {type: Number, default: 0},
+        salarioLiquido: {type: Number},
+        aliquotaINSS: {type: Number},
+        aliquotaIRPF: {type: Number},
+        descontoINSS: {type: Number},
+        descontoIRPF: {type: Number},
         outrosDescontos: {type: Number, default: 0},
-        salarioLiquido: {type: Number}
+        totalDescontos: {type: Number},
+        profissao: {type: String}
     },
     {
         versionKey: false
